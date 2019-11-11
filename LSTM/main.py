480f399f630594a088b09542d634fc384721a525
Tmennick <timtor@hotmail.de>
2019-11-11 17:50:28 +0100

    first try LSTM

---
+++ LSTM/main.py
@@ -0,0 +1,148 @@
+import numpy as np
+import pandas as pd
+
+import os
+#print(os.listdir("./datasets"))
+
+from keras.callbacks import LambdaCallback
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.optimizers import RMSprop
+from keras.optimizers import Adam
+from keras.utils.data_utils import get_file
+import random
+import sys
+import io
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./datasets/Donald-Tweets.csv")
+print(df.shape)
+print(df.head())
+
+text = df["Tweet_Text"].str.lower()
+
+print("Before:")
+print(text[0])
+text = text.map(lambda s: ' '.join([x for x in s.split() if 'http' not in x]))
+print("After:")
+print(text[0])
+
+print('max tweet len:',text.map(len).max())
+print('min tweet len:',text.map(len).min())
+print(text.map(len).hist())
+
+print(len(text))
+text = text[text.map(len)>60]
+print(len(text))
+
+chars = sorted(list(set(''.join(text))))
+print('total chars:', len(chars))
+print(chars)
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+for c in chars[-19:]:
+    print("\nCHAR: ", c)
+    smple = [x for x in text if c in x]
+    print(random.sample(smple,min(3,len(smple))))
+    text = text.str.replace(c, "")
+
+chars = sorted(list(set(''.join(text))))
+print('total chars:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+maxlen = 40
+step = 1
+sentences = []
+next_chars = []
+for x in text:
+    for i in range(0, len(x) - maxlen, step):
+        sentences.append(x[i: i + maxlen])
+        next_chars.append(x[i + maxlen])
+print("nb sequences: ", len(sentences))
+
+
+print('Vectorization...')
+x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+print("X: ")
+print(x)
+print("Y: ")
+print(y)
+
+
+print('Build model...')
+model = Sequential()
+model.add(LSTM(128, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(Dropout(0.5))
+model.add(LSTM(128))
+model.add(LSTM(128))
+model.add(Dropout(0.5))
+model.add(Dense(len(chars), activation='softmax'))
+
+# optimizer = RMSprop(lr=0.01)
+optimizer = Adam()
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+model.summary()
+
+
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def on_epoch_end(epoch, _):
+    # Function invoked at end of each epoch. Prints generated text.
+    print()
+    print('----- Generating text after Epoch: %d' % epoch)
+    
+#     start_index = random.randint(0, len(text) - maxlen - 1)
+    tweet = np.random.choice(text) # select random tweet
+    start_index = 0
+
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print('----- diversity:', diversity)
+
+        generated = ''
+        sentence = tweet[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(120):
+            x_pred = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x_pred[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
+
+epochs = 20
+
+print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+
+model.fit(x, y,
+          batch_size=128,
+          epochs=epochs,
+          callbacks=[print_callback])
